8d7e72a9f1a3ca332537dbd5720b5534cd1e67c0
hei-jack <64543432+hei-jack@users.noreply.github.com>
2021-07-25 15:35:39 +0800

    更新安装引导功能

--- public/install/controller.php
+++ public/install/controller.php
@@ -1,10 +1,18 @@
 <?php
 //设置报错级别
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-//引入类
+
+//定义路径分隔符常量
+define('DS',DIRECTORY_SEPARATOR);
+
+//定义根目录
+define('ROOT_PATH',dirname(dirname(__DIR__)));
+
+//引入助手类
 require_once('./Helper.php');
 
-//实例化安装助手函数
+
+//实例化安装助手类
 $helper = new Helper();
 //拦截非post请求
 if($_SERVER['REQUEST_METHOD'] !== 'POST') $helper->json(405,'非法请求！');
@@ -13,21 +21,17 @@ $action = @$_GET['a'];
 $data = @$_POST;
 if($action !== null && $data !== null){
   //如果两个参数都不为空 开始进行下一步
-  if($action === 'step1'){
-    //如果是步骤一 环境检测
-    
-  }else if($action === 'step2'){
-
-  }else if($action === 'step3'){
-
-  }else if($action === 'finsh'){
-
-  }
   switch(strval($action)){
     case 'index':
       $helper->getInfo();
       break;
-    case 'step1':
-      $helper->step1();
+    case 'step2':
+      $helper->step2($data);
+      break;
+    case 'step3':
+      $helper->step3($data);
+      break;
+    case 'finsh':
+      $helper->finsh();
   }
 }
